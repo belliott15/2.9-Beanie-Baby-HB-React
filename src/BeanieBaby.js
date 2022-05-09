e4b570065eae85350907c0d79e68f480a927bacd
Beau Elliott <belliott1516@gmail.com>
2022-05-09 19:33:33 -0400

    added animation on hover

--- src/BeanieBaby.js
+++ src/BeanieBaby.js
@@ -4,7 +4,7 @@ export default function BeanieBaby({ beanieBaby }) {
   return (
     // this should contain a react-router-dom Link to the detail page for this particular beanie baby.
     // it should also render the beanie baby's image and show the beanie baby's name
-    <Link to={`/beanies/${beanieBaby.id}`}>
+    <Link className="card" to={`/beanies/${beanieBaby.id}`}>
       <div className='beanie-baby'>
         <p>{beanieBaby.title}</p>
         <p>DOB: {beanieBaby.birthday}</p>
